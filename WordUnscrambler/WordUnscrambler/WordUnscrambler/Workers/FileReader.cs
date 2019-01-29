e09a1347460e948297213c48a1c1b2ff25643085
Ron <38900227+jamesron22@users.noreply.github.com>
2019-01-28 22:30:49 -0800

    Add files via upload

---
+++ WordUnscrambler/WordUnscrambler/WordUnscrambler/Workers/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WordUnscrambler.Workers
+{
+    class FileReader
+    {
+        public string[] Read(string filename)
+        {
+            //Creates a string array
+            string[] fileContent;
+            try
+            {
+                //Reads words from file
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return fileContent;
+        }
+    }
+}
